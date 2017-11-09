7e2b5acfb228186e294b919eb4662852007fc727
Edmund Teo <edmund@Edmunds-Mac.local>
2017-11-09 11:19:03 +0800

    using object

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+// data for cards
+var cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+},
+{	
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+}
+];
+
 var cardsInPlay = [];
 
+// function to check if cards match
 var checkForMatch = function() {
 if (cardsInPlay[0] === cardsInPlay[1]) {
 	console.log('You found a match!');
@@ -9,14 +33,17 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 }
 };
 
+// function to flip card
 var flipCard = function(cardId) {
-console.log('User flipped ' + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log('User flipped ' + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
 
+// exercute function 2 times 
 if (cardsInPlay.length === 2) {
 checkForMatch();
 }
 };
 
-flipCard(3);
+// calling function to flip card
+flipCard(0);
 flipCard(2);
